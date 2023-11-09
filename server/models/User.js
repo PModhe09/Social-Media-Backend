d4563e93a16b8537ebddb3c8bb2f09414b3a8502
PrathmeshModhe9 <lastry2001@gmail.com>
2023-11-09 12:37:23 +0530

    initial commit

---
+++ server/models/User.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    maskedName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+      unique:true,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    location: String,
+    college: String,
+    batch:String,
+    viewedProfile: Number,
+    impressions: Number,
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+export default User;
